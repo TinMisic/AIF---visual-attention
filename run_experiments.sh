254bd98446f955111cf67796392302a47420efdc
Tin Misic <misic.tin@gmail.com>
2025-02-28 23:45:43 +0100

    SOme experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,10 +2,10 @@
 
 # List of commands to run consecutively
 commands=(
-    "echo 'Experiment 1: Endogenous Valid' && ros2 run aif_model auto_trial --trials 1 --init 10 --cue 50 --coa 100 --max 1000 --endo true --valid true --action false"
-    "echo 'Experiment 2: Endogenous Invalid' && ros2 run aif_model auto_trial --trials 1 --init 10 --cue 50 --coa 100 --max 1000 --endo true --valid flase --action false"
-    "echo 'Experiment 3: Exogenous Valid' && ros2 run aif_model auto_trial --trials 1 --init 10 --cue 50 --coa 100 --max 1000 --endo false --valid true --action false"
-    "echo 'Experiment 4: Exogenous Invalid' && ros2 run aif_model auto_trial --trials 1 --init 10 --cue 50 --coa 100 --max 1000 --endo false --valid false --action false"
+    "echo 'Experiment 1: Endogenous Valid' && ros2 run aif_model auto_trial --trials 200 --init 10 --cue 50 --coa 150 --max 1000 --endo true --valid true --action false"
+    "echo 'Experiment 2: Endogenous Invalid' && ros2 run aif_model auto_trial --trials 200 --init 10 --cue 50 --coa 150 --max 1000 --endo true --valid flase --action false"
+    "echo 'Experiment 3: Exogenous Valid' && ros2 run aif_model auto_trial --trials 200 --init 10 --cue 50 --coa 150 --max 1000 --endo false --valid true --action false"
+    "echo 'Experiment 4: Exogenous Invalid' && ros2 run aif_model auto_trial --trials 200 --init 10 --cue 50 --coa 150 --max 1000 --endo false --valid false --action false"
 )
 
 # Run each command sequentially
